12e824cd3bb181cca654464aed536d2256ec7749
조문기_T4207 <siryuon@gmail.com>
2022-11-11 10:34:59 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,26 +1,30 @@
 import sys
 
 sys.path.append('model')
-from typing import Callable, Dict, List, NoReturn, Tuple
-import numpy as np
+
 import streamlit as st
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
+
+from model import Model
 
 @st.cache(allow_output_mutation=True)
 def load_model():
     model_name = 'snunlp/KR-ELECTRA-discriminator'
     config = AutoConfig.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True, max_length=100)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, config=config)
+    model = Model.load_from_checkpoint(checkpoint_path='model.ckpt')
     return model, tokenizer
 
 def run(tokenizer, model, st1, st2):
     resultLst = ['비슷한 문장이 아닌 것 같은데?', '비슷한 문장인 것 같아!']
     text = st1 + '[SEP]' + st2
-    token = tokenizer.encode_plus(text, add_special_tokens=True, return_tensors='pt', max_length=100,
-                                      truncation=True)
-    pred = model(token['input_ids'])[0]
-    print(pred)
-    result = pred.argmax().item()
+    token = tokenizer(text, add_special_tokens=True, return_tensors='pt', max_length=100, truncation=True)['input_ids']
+    pred = model(token)
 
-    return resultLst[result]
+    label = abs(round(float(pred), 1))
+    if label < 0 :
+        label = 0.0
+    elif label >= 5:
+        label = 5.0
+
+    return label
